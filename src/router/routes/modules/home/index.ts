868aa97a68c138eb36139e90f8f8fe586588bf34
yanzhangshuai <yanzhangshuai@126.com>
2021-07-29 15:15:58 +0800

    refactor:重命名pages目录为page

--- src/router/routes/modules/home/index.ts
+++ src/router/routes/modules/home/index.ts
@@ -3,7 +3,7 @@ import Demo1Router from './demo1';
 const router: RouteRecordRaw = {
   path: '/home',
   name: 'home',
-  component: () => import('@/pages/home/index.vue'),
+  component: () => import('@/page/home/index.vue'),
   meta: {
     auth: true
   },
@@ -16,7 +16,7 @@ const router: RouteRecordRaw = {
     {
       path: 'demo2',
       name: 'home-demo2',
-      component: () => import('@/pages/home/demo2/index.vue')
+      component: () => import('@/page/home/demo2/index.vue')
     }
   ]
 };
