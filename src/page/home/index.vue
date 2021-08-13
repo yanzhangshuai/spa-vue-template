1f21de383a70195119a81b1c3bec78ae9752eee9
yanzhangshuai <yanzhangshuai@126.com>
2021-08-13 13:36:19 +0800

    feat:设计Service

--- src/page/home/index.vue
+++ src/page/home/index.vue
@@ -8,8 +8,32 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { useHttp } from '@/service';
+
 export default defineComponent({
   components: {},
-  setup() {}
+  setup() {
+    const http = useHttp();
+    http
+      .get<string>(
+        '/api/app/hello',
+        {
+          id: 1,
+          name: '123',
+          d: [1, 2],
+          m: {
+            '1': '1'
+          }
+        },
+        {}
+      )
+      .then((res) => {
+        console.log('res', res);
+      })
+      .catch((err) => {
+        console.log('err', err);
+      });
+    return {};
+  }
 });
 </script>
---
+++ src/service/index.ts
@@ -0,0 +1,17 @@
+import { App } from 'vue';
+import { Http } from './http/index';
+import { setupInterceptor } from './interceptor/index';
+
+let http: Http;
+
+export function setupService(app: App<Element>): App<Element> {
+  http = new Http({ request: { ignoreCancelToken: false } });
+
+  setupInterceptor(http.interceptor);
+
+  return app;
+}
+
+export function useHttp(): Http {
+  return http;
+}
