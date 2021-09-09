bf01d37a428a5912e647659bd17e182e47616371
yanzhangshuai <yanzhangshuai@126.com>
2021-09-09 16:52:59 +0800

    build:升级buck-storage

--- src/page/app.vue
+++ src/page/app.vue
@@ -5,6 +5,7 @@
 </template>
 
 <script lang="ts">
+import { useStorage } from 'util/storage';
 import { defineComponent } from 'vue';
 import { useTitle } from 'hook/web/title';
 
@@ -12,6 +13,8 @@ export default defineComponent({
   name: 'App',
   setup() {
     useTitle();
+    const storage = useStorage();
+    console.log("  storage.get('name')", storage.get('name'));
   }
 });
 </script>
--- src/page/home/demo1/index.vue
+++ src/page/home/demo1/index.vue
@@ -1,7 +1,15 @@
 <template>
   <div>我是demo1</div>
   <button @click="$router.push('/home/demo2')">Go To Demo2</button>
+  <setup-demo ref="setupDemoRef" v-model:name="name"></setup-demo>
   <tsx-demo />
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { onMounted, ref, unref } from 'vue';
+const setupDemoRef = ref(null);
+onMounted(() => {
+  unref(setupDemoRef).onBtn();
+});
+const name = ref('123');
+</script>
