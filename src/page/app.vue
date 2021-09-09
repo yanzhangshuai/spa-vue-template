107ebcb6ec8427364a6028ae81ea58a5eca55675
yanzhangshuai <yanzhangshuai@126.com>
2021-09-09 18:24:20 +0800

    refactor:vue组件改为setup文件模式

--- src/page/app.vue
+++ src/page/app.vue
@@ -4,17 +4,11 @@
   </config-provider>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 import { useStorage } from 'util/storage';
-import { defineComponent } from 'vue';
 import { useTitle } from 'hook/web/title';
 
-export default defineComponent({
-  name: 'App',
-  setup() {
-    useTitle();
-    const storage = useStorage();
-    console.log("  storage.get('name')", storage.get('name'));
-  }
-});
+useTitle();
+const storage = useStorage();
+console.log("  storage.get('name')", storage.get('name'));
 </script>
--- src/page/home/index.vue
+++ src/page/home/index.vue
@@ -3,10 +3,4 @@
   <router-view />
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-
-export default defineComponent({
-  setup() {}
-});
-</script>
+<script lang="ts" setup></script>
