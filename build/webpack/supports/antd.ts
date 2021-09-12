f8110d98fe1032ad6d6219395ec70bd3ca315c15
yanzhangshuai <yanzhangshuai@126.com>
2021-09-12 15:50:27 +0800

    build:升级antd

--- build/webpack/supports/antd.ts
+++ build/webpack/supports/antd.ts
@@ -2,7 +2,7 @@ import { Configuration } from 'webpack';
 import AntdDayjsWebpackPlugin from 'antd-dayjs-webpack-plugin';
 import { resolve } from '../../utils';
 export function antdSupport(needed: boolean): Configuration {
-  const alias = {};
+  const alias: Record<string, string> = {};
 
   needed && (alias['@ant-design/icons/lib/dist$'] = resolve('src/component/antd/icons.ts'));
 
