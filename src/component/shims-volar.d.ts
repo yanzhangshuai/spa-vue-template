58d1e947c7f140b74212c375210b4c60f7d8e0b6
yxs97 <yanzhangshuai@126.com>
2021-10-28 11:46:03 +0800

    refactor:整理代码

--- src/component/shims-volar.d.ts
+++ src/component/shims-volar.d.ts
@@ -3,6 +3,7 @@ declare module 'vue' {
     'ref-demo': typeof import('@/component/modules/base/ref-demo/index.vue')['default'];
     'ref-setup-demo': typeof import('@/component/modules/base/ref-setup-demo/index.vue')['default'];
     'config-provider': typeof import('@/component/modules/base/config-provider/index.vue')['default'];
+    layout: typeof import('@/component/modules/base/layout/index.vue')['default'];
   }
 }
 export {};
