9b2e0eb0d1bf03fc2023479f652b07148cbbbff0
yxs97 <yanzhangshuai@126.com>
2022-02-14 11:09:38 +0800

    feat: 页面可访问项目版本

--- src/page/home/demo1/index.vue
+++ src/page/home/demo1/index.vue
@@ -7,6 +7,7 @@
   <p>文件服务器地址 :{{ $globalProps.FILE_PATH_PREFIX }}</p>
   <p>当前环境 :{{ $globalProps.DEV }}</p>
   <p>当前时间 :{{ $globalProps.dateFormat($window.Date.now()) }}</p>
+  <p>当前版本 :{{ $globalProps.VERSION }}</p>
 </template>
 
 <script lang="ts" setup>
