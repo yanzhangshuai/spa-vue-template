80d0fa2fc9dbe4e82d9c09717a9938b1bfce4996
yxs97 <yanzhangshuai@126.com>
2022-02-15 11:57:29 +0800

    feat: 添加unplugin-vue-components插件

--- build/webpack/support.ts
+++ build/webpack/support.ts
@@ -1,20 +1,21 @@
 import { Configuration } from 'webpack';
 import { merge as webpackMerge } from 'webpack-merge';
 import { Env } from '../type';
-import { compressSupport } from './supports/compress';
+import { vueSupport } from './supports/vue';
 import { htmlSupport } from './supports/html';
+import { styleSupport } from './supports/style';
 import { reportSupport } from './supports/report';
 import { scriptSupport } from './supports/script';
-import { styleSupport } from './supports/style';
 import { variableSupport } from './supports/variable';
-import { vueSupport } from './supports/vue';
-
+import { compressSupport } from './supports/compress';
+import { autoVolarSupport } from './supports/auto-volar';
 export function support(isBuild: boolean, env: Env): Configuration {
   const supports = [
     vueSupport(),
     scriptSupport(),
-    variableSupport(isBuild, env),
+    autoVolarSupport(),
     styleSupport(isBuild),
+    variableSupport(isBuild, env),
     htmlSupport(isBuild, env.WEBPACK_APP_TITLE),
     env.WEBPACK_REPORT && reportSupport(),
     isBuild && compressSupport(env.WEBPACK_BUILD_COMPRESS, true)
