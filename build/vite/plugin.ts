be76ee688041245007ed2266c839235819244629
yxs97 <yanzhangshuai@126.com>
2022-02-15 13:51:18 +0800

    fix: 修复不支持按需加载tsx方式使用的antdv框样式

--- build/vite/plugin.ts
+++ build/vite/plugin.ts
@@ -1,5 +1,6 @@
 import { Plugin } from 'vite';
 import { Env } from '../type';
+import { uiPlugin } from './plugins/ui';
 import { vuePlugin } from './plugins/vue';
 import { htmlPlugin } from './plugins/html';
 import { reportPlugin } from './plugins/report';
@@ -10,6 +11,7 @@ export function createVitePlugins(viteEnv: Env, isBuild: boolean): Array<Plugin 
   return [
     vuePlugin(viteEnv.VITE_SUPPORT_JSX),
     htmlPlugin(isBuild, viteEnv.VITE_APP_TITLE),
+    uiPlugin(),
     autoVolarPlugin(),
     viteEnv.VITE_REPORT && reportPlugin(),
     isBuild && compressPlugin(viteEnv.VITE_BUILD_COMPRESS, viteEnv.VITE_BUILD_COMPRESS_DELETE_ORIGIN_FILE)
