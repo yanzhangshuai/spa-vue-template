a46a5d08d7e88d204fef7777c56604f1d3b5db97
yxs97 <yanzhangshuai@126.com>
2022-02-17 16:39:29 +0800

    refactor: 重构代码

--- build/vite/plugin.ts
+++ build/vite/plugin.ts
@@ -1,5 +1,6 @@
 import { Plugin } from 'vite';
 import { Env } from '../type';
+import { uiPlugin } from './plugins/ui';
 import { vuePlugin } from './plugins/vue';
 import { cssPlugin } from './plugins/css';
 import { htmlPlugin } from './plugins/html';
@@ -7,5 +8,5 @@ import { reportPlugin } from './plugins/report';
 import { compressPlugin } from './plugins/compress';
 
 export function createVitePlugins(isBuild: boolean, viteEnv: Env): Array<Plugin | Plugin[]> {
-  return [vuePlugin(isBuild, viteEnv), cssPlugin(), htmlPlugin(isBuild, viteEnv), reportPlugin(isBuild, viteEnv), compressPlugin(isBuild, viteEnv)];
+  return [vuePlugin(isBuild, viteEnv), cssPlugin(), uiPlugin(isBuild, viteEnv), htmlPlugin(isBuild, viteEnv), reportPlugin(isBuild, viteEnv), compressPlugin(isBuild, viteEnv)];
 }
--- build/vite/plugins/ui.ts
+++ build/vite/plugins/ui.ts
@@ -1,21 +1,15 @@
-import type { Plugin } from 'vite';
 import { createStyleImportPlugin, AndDesignVueResolve } from 'vite-plugin-style-import';
+import { PluginFn } from '../type';
 
-export function uiPlugin(): Array<Plugin> {
-  const plugins: Array<Plugin> = [];
-
-  plugins.push(
-    createStyleImportPlugin({
-      resolves: [AndDesignVueResolve()],
-      libs: [
-        {
-          libraryName: 'ant-design-vue',
-          esModule: true,
-          resolveStyle: (name) => `ant-design-vue/es/${name}/style/index`
-        }
-      ]
-    })
-  );
-
-  return plugins;
-}
+export const uiPlugin: PluginFn = () => {
+  return createStyleImportPlugin({
+    resolves: [AndDesignVueResolve()],
+    libs: [
+      {
+        libraryName: 'ant-design-vue',
+        esModule: true,
+        resolveStyle: (name) => `ant-design-vue/es/${name}/style/index`
+      }
+    ]
+  });
+};
