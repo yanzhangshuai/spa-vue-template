35de698b833d524ee794b518ab0402e3d5925060
yxs97 <yanzhangshuai@126.com>
2022-03-11 18:12:38 +0800

    build: 升级库版本

--- src/component/shims-volar.d.ts
+++ src/component/shims-volar.d.ts
@@ -1,7 +1,6 @@
 declare module 'vue' {
   export interface GlobalComponents {
     'config-provider': typeof import('./modules/config-provider/index.vue')['default'];
-    'tsx-demo': typeof import('./modules/base/tsx-demo/index')['default'];
     'vue-demo': typeof import('./modules/base/vue-demo/index.vue')['default'];
   }
 }
